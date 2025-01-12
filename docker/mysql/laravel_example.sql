979cfc05a4a0436550911558d6cbf5d6b4047ffe
nuralazmi <nuralazmi@gmail.com>
2025-01-13 00:08:49 +0300

    delete symfony

--- docker/mysql/laravel_example.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS is_case_laravel;
-
-USE is_case_laravel;
-
-CREATE TABLE IF NOT EXISTS docker_laravel_test_table (id INT AUTO_INCREMENT PRIMARY KEY);
--- docker/mysql/symfony_example.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS is_case_symfony;
-
-USE is_case_symfony;
-
-CREATE TABLE IF NOT EXISTS docker_symfony_test_table (id INT AUTO_INCREMENT PRIMARY KEY);
